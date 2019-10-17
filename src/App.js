698b904616fe29772793a4105b9baba2573c1e1a
bitiho <biti13012000@gmail.com>
2019-10-17 11:00:43 +0700

    buoi4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SinhVien from './components/SinhVien/SinhVien';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <SinhVien hoten="Ronaldo" tuoi="35">
+            Sở thích: bóng đá , nghe nhạc, đi chơi
+          </SinhVien>
+          <SinhVien hoten="Messi" tuoi="32">
+          Sở thích: bóng đá , nghe nhạc, đi chơi
+          </SinhVien>
+        </header>
+      </div>
+    );
+  }
 }
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
